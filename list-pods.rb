dca67910ac6a0a094e5cad704d4e8c36fc4dfa01
David Salgado <david.salgado@digital.justice.gov.uk>
2019-05-23 11:49:28 +0100

    Fetch list of defined namespaces from env-repo

--- list-pods.rb
+++ list-pods.rb
@@ -3,8 +3,17 @@
 require 'bundler/setup'
 require 'openid_connect'
 require 'kubeclient'
+require 'open-uri'
+require 'json'
 require 'pp'
 require 'pry-byebug'
+
+URL = 'https://api.github.com/repos/ministryofjustice/cloud-platform-environments/contents/namespaces/live-1.cloud-platform.service.justice.gov.uk'
+
+content = open(URL).read
+git_namespaces = JSON.parse(content).map {|hash| hash.fetch('name')}
+pp git_namespaces
+exit
 
 config = Kubeclient::Config.read(ENV.fetch('KUBECONFIG'))
 context = config.context
