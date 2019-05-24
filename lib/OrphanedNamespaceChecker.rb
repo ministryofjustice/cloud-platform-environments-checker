cdc15a49e11b87938fd8d1e9f0e184e05c9fd9d6
David Salgado <david.salgado@digital.justice.gov.uk>
2019-05-24 11:31:53 +0100

    Start separating classes into their own files

---
+++ lib/OrphanedNamespaceChecker.rb
@@ -0,0 +1,10 @@
+require 'bundler/setup'
+require 'openid_connect'
+require 'kubeclient'
+require 'open-uri'
+require 'aws-sdk-s3'
+require 'json'
+
+require './lib/GithubNamespaceLister'
+
+require 'pp'
--- orphaned_namespaces.rb
+++ orphaned_namespaces.rb
@@ -1,30 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'bundler/setup'
-require 'openid_connect'
-require 'kubeclient'
-require 'open-uri'
-require 'aws-sdk-s3'
-require 'json'
-
-require 'pp'
-
-class GithubNamespaceLister
-  attr_reader :env_repo, :cluster_name
-
-  def initialize(args)
-    @env_repo = args.fetch(:env_repo)
-    @cluster_name = args.fetch(:cluster_name)
-  end
-
-  def namespace_names
-    env_repo_namespace_path = "https://api.github.com/repos/ministryofjustice/#{env_repo}/contents/namespaces/#{cluster_name}"
-    content = open(env_repo_namespace_path).read
-    names = JSON.parse(content).map { |hash| hash.fetch('name') }
-    raise "No github repositories returned. Aborting" if names.empty?
-    names
-  end
-end
+require './lib/OrphanedNamespaceChecker'
 
 class CloudPlatformOrphanNamespaces
 
