e95efde19093062d99eefdaa6054292209270912
David Salgado <david.salgado@digital.justice.gov.uk>
2019-05-24 15:59:00 +0100

    Move classes into lib/orphaned_namespace_checker/

--- lib/orphaned_namespace_checker.rb
+++ lib/orphaned_namespace_checker.rb
@@ -5,9 +5,9 @@ require 'open-uri'
 require 'aws-sdk-s3'
 require 'json'
 
-require './lib/cloud_platform_orphaned_namespaces'
-require './lib/github_namespace_lister'
-require './lib/tf_state_namespace_lister'
-require './lib/cluster_namespace_lister'
+require './lib/orphaned_namespace_checker/cloud_platform_orphaned_namespaces'
+require './lib/orphaned_namespace_checker/github_namespace_lister'
+require './lib/orphaned_namespace_checker/tf_state_namespace_lister'
+require './lib/orphaned_namespace_checker/cluster_namespace_lister'
 
 require 'pp'
