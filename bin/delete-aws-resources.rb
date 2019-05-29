9a29beaa99d67c706c381ada523dddc9d3f9062b
David Salgado <david.salgado@digital.justice.gov.uk>
2019-05-29 16:31:16 +0100

    Add ruby script to plan deletion of AWS resources
    Given a namespace name, this script initialises
    terraform, in preparation for running a 
    `terraform apply` to delete all the AWS resources
    belonging to the namespace.
    
    The script assumes the same environment variables
    are in scope as used by the 
    `orphaned_namespaces.rb` script.

---
+++ bin/delete-aws-resources.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+require "#{File.dirname(__FILE__)}/../lib/orphaned_namespace_checker"
+
+# This is the 'empty' main.tf file that we add, by default, to any namespaces users create
+CANONICAL_MAIN_TF_URL = 'https://raw.githubusercontent.com/ministryofjustice/cloud-platform-environments/master/namespace-resources/resources-main-tf'
+
+# TODO: get from env. vars.
+PIPELINE_STATE_BUCKET = 'moj-cp-k8s-investigation-environments-terraform'
+PIPELINE_STATE_REGION = 'eu-west-1'
+PIPELINE_CLUSTER = 'cloud-platform-live-0.k8s.integration.dsd.io'
+
+def main(namespace)
+  puts
+  puts "About to delete AWS resources for namespace: ${namespace}"
+  puts
+
+  # Ensure we have AWS credentials
+  ENV.fetch('TFSTATE_AWS_ACCESS_KEY_ID')
+  ENV.fetch('TFSTATE_AWS_SECRET_ACCESS_KEY')
+
+  system("rm -rf .terraform main.tf") # clean up any leftover artefacts from prior invocations
+  add_main_tf
+  tf_init namespace
+  system('terraform plan')
+end
+
+def add_main_tf
+  content = open(CANONICAL_MAIN_TF_URL).read
+  raise "Couldn't retrieve main.tf from #{CANONICAL_MAIN_TF_URL}" if content.empty?
+  File.open('main.tf', 'w') {|f| f.puts content}
+end
+
+def tf_init(namespace)
+  cmd = <<~EOF
+  terraform init \
+    -backend-config="access_key=${TFSTATE_AWS_ACCESS_KEY_ID}" \
+    -backend-config="secret_key=${TFSTATE_AWS_SECRET_ACCESS_KEY}" \
+    -backend-config="bucket=#{PIPELINE_STATE_BUCKET}" \
+    -backend-config="key=#{PIPELINE_CLUSTER}/#{namespace}/terraform.tfstate" \
+    -backend-config="region=#{PIPELINE_STATE_REGION}"
+  EOF
+  system cmd
+end
+
+main ARGV.shift
