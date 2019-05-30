515ba965f2c8cf1005c6a9fb0ea9b54d2a3fa2f9
David Salgado <david@digitalronin.com>
2019-05-30 16:33:07 +0100

    Require pry-byebug in the spec helper

--- spec/cloud_platform_orphaned_namespaces_spec.rb
+++ spec/cloud_platform_orphaned_namespaces_spec.rb
@@ -1,6 +1,17 @@
 RSpec.describe CloudPlatformOrphanNamespaces do
-  let(:params) { { github_lister: github_lister } }
+  let(:params) { {
+    cluster_name:    'foo',
+    kubeconfig:      double(Object, fetch:            true),
+    tfstate_lister:  double(Object, namespaces:       []),
+    cluster_lister:  double(Object, namespace_names:  []),
+    github_lister:   github_lister,
+  } }
+
   subject(:checker) { described_class.new(params) }
+
+  before do
+    allow_any_instance_of(Kubeconfig).to receive(:fetch_and_store).and_return(true)
+  end
 
   context "when github lister returns empty list" do
     let(:github_lister) { double(GithubNamespaceLister, namespace_names: []) }
