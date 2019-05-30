c5f67466f1a736c1be76388c1c64bde019f9616e
David Salgado <david@digitalronin.com>
2019-05-30 14:39:11 +0100

    Clarify the script code
    Building the string outside of the File.open
    block makes it easier to see what's going on.

--- bin/orphaned_namespaces.rb
+++ bin/orphaned_namespaces.rb
@@ -27,15 +27,15 @@ tfstate = {
   bucket_prefix: env('BUCKET_PREFIX')
 }
 
+result = CloudPlatformOrphanNamespaces.new(
+  kubeconfig: kubeconfig,
+  tfstate:    tfstate
+).report
+
 # Concourse will create the 'output' directory during the
 # 'check-environments' pipeline task. It will do so as root,
 # so this script also needs to run as root, or it will not
 # be able to write to a file in that directory.
 # Concourse seems to have a baked-in assumption that it, and
 # any containers it runs, will run as root.
-File.open('./output/check.txt', 'w') do |f|
-  f.puts CloudPlatformOrphanNamespaces.new(
-    kubeconfig: kubeconfig,
-    tfstate:    tfstate
-  ).report
-end
+File.open('./output/check.txt', 'w') { |f| f.puts(result) }
