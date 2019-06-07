04003133232fa1360a75a1be08d124174abd94c5
David Salgado <david.salgado@digital.justice.gov.uk>
2019-06-07 17:02:57 +0100

    Tweak spec for readability

--- spec/tf_state_namespace_lister_spec.rb
+++ spec/tf_state_namespace_lister_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe TFStateNamespaceLister do
   let(:bucket_contents) { double(:bucket_contents, contents: contents) }
+
   let(:io) { double(:io, read: state) }
   let(:s3obj) { double(:s3obj, body: double(:io, read: '{ "modules": [] }')) }
   let(:s3client) { double(:s3client, list_objects: bucket_contents, get_object: s3obj) }
+
   let(:bucket_prefix) { 'qqq' }
+  let(:tfstate_class) { TFStateNamespaceLister::TFState }
 
   let(:params) { {
     bucket: double(:bucket),
@@ -34,8 +37,9 @@ RSpec.describe TFStateNamespaceLister do
       { type: 'aws_iam_user', id: 'ecr-user-3071a3145d675234' },
       { type: 'aws_iam_user_policy', id: 'ecr-user-3071a3145d675234:ecr-read-write' },
     ] }
+
     let(:tfstate) {
-      TFStateNamespaceLister::TFState.new('money-to-prisoners-prod', resources)
+      tfstate_class.new('money-to-prisoners-prod', resources)
     }
 
     it "lists AWS resources in namespace" do
@@ -45,24 +49,25 @@ RSpec.describe TFStateNamespaceLister do
   end
 
   context "given a list of s3 bucket objects" do
-    let(:obj1) { double(Aws::S3::Types::Object, key: key1) }
-    let(:obj2) { double(Aws::S3::Types::Object, key: key2) }
-    let(:obj3) { double(Aws::S3::Types::Object, key: key3) }
+    let(:obj1) { double(:s3obj, key: key1) }
+    let(:obj2) { double(:s3obj, key: key2) }
+    let(:obj3) { double(:s3obj, key: key3) }
 
     let(:contents) { [obj1, obj2, obj3] }
 
     let(:namespaces) { [
-      TFStateNamespaceLister::TFState.new('weekly-app-deploy-oa', []),
-      TFStateNamespaceLister::TFState.new('whereabouts-dev', []),
-      TFStateNamespaceLister::TFState.new('vv-myapp-dev', []),
+      tfstate_class.new('weekly-app-deploy-oa',  []),
+      tfstate_class.new('whereabouts-dev',       []),
+      tfstate_class.new('vv-myapp-dev',          []),
     ] }
 
     context "in live0" do
+      let(:bucket_prefix) { 'cloud-platform-live-0.k8s.integration.dsd.io/' }
+
       let(:key1) { "cloud-platform-live-0.k8s.integration.dsd.io/weekly-app-deploy-oa/terraform.tfstate" }
       let(:key2) { "cloud-platform-live-0.k8s.integration.dsd.io/whereabouts-dev/terraform.tfstate" }
       let(:key3) { "cloud-platform-live-0.k8s.integration.dsd.io/vv-myapp-dev/terraform.tfstate" }
 
-      let(:bucket_prefix) { 'cloud-platform-live-0.k8s.integration.dsd.io/' }
 
       it "extracts namespace names" do
         expect(lister.namespaces).to eq(namespaces)
@@ -70,11 +75,11 @@ RSpec.describe TFStateNamespaceLister do
     end
 
     context "in live1" do
+      let(:bucket_prefix) { '' }
+
       let(:key1) { "weekly-app-deploy-oa/terraform.tfstate" }
       let(:key2) { "whereabouts-dev/terraform.tfstate" }
       let(:key3) { "vv-myapp-dev/terraform.tfstate" }
-
-      let(:bucket_prefix) { '' }
 
       it "extracts namespace names" do
         expect(lister.namespaces).to eq(namespaces)
