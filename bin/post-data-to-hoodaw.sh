a30f90c629efae84e16dfe813e3ed7581f5bd3a0
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-09-29 14:05:24 +0100

    rename file to hosted_services which needs to be run

--- bin/post-data-to-hoodaw.sh
+++ bin/post-data-to-hoodaw.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-curl -H "X-API-KEY: ${HOODAW_API_KEY}" -d "$(/app/bin/cp_hosted_namespaces.rb)" ${HOODAW_HOST}/hosted_services
+curl -H "X-API-KEY: ${HOODAW_API_KEY}" -d "$(/app/bin/hosted_services.rb)" ${HOODAW_HOST}/hosted_services
